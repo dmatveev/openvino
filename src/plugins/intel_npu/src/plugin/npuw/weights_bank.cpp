dea097ab838602bf2ee201691cbd91eddf625fcf
Dmitry Matveev <dmitry.matveev@intel.com>
2024-09-02 09:55:31 +0100

    NPUW L0: Integrate DQ with remote tensors (WIP)
    Don't work well, to be continued

--- src/plugins/intel_npu/src/plugin/npuw/weights_bank.cpp
+++ src/plugins/intel_npu/src/plugin/npuw/weights_bank.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "weights_bank.hpp"
+#include "logging.hpp"
 
 using ov::npuw::weights::Bank;
 
@@ -34,12 +35,8 @@ ov::Tensor Bank::update(const ov::Tensor& tensor) {
     }
 
     std::lock_guard<std::mutex> guard(m_mutex);
-
-    if (m_bank.find(tensor.data()) == m_bank.end()) {
-        // need to allocate first
-        m_bank[tensor.data()] = tensor;
-    }
-
+    m_bank[tensor.data()] = tensor;
+    m_bytes.total_registered += tensor.get_byte_size();
     return tensor;
 }
 
@@ -59,7 +56,7 @@ ov::Tensor Bank::get(const ov::Tensor& tensor, const std::string& device) {
         OPENVINO_THROW("Unknown tensor in weights bank allocation!");
     }
 
-    // If target device is CPU - just reuse the default bank
+    // If target device is CPU - just reuse the default bank.
     if (device == "CPU") {
         return iter_cpu->second;
     }
@@ -72,13 +69,55 @@ ov::Tensor Bank::get(const ov::Tensor& tensor, const std::string& device) {
         return iter_device->second;
     }
 
-    // Allocation needed
-    m_remote_ctx = m_core->get_default_context(device)._ptr;
-    auto remote_tensor = m_remote_ctx->create_host_tensor(tensor.get_element_type(), tensor.get_shape());
-    auto allocated_tensor = ov::make_tensor(remote_tensor);
-    tensor.copy_to(allocated_tensor);
-    device_bank[tensor.data()] = allocated_tensor;
-    return allocated_tensor;
+    // Allocation needed. Still device if allocate it on device or not.
+    const auto tbytes = tensor.get_byte_size();
+    m_bytes.pinned_total += tbytes;
+
+    if (decide_for_dev(tensor, device)) {
+        m_remote_ctx = m_core->get_default_context(device)._ptr;
+        auto remote_tensor = m_remote_ctx->create_host_tensor(tensor.get_element_type(),
+                                                              tensor.get_shape());
+        auto allocated_tensor = ov::make_tensor(remote_tensor);
+        tensor.copy_to(allocated_tensor);
+
+        m_bytes.pinned_dev += tbytes;
+        return (device_bank[tensor.data()] = allocated_tensor);
+    }
+    // ..Not ok - still return a cpu tensor. Store it in the device_bank
+    // instead of the remote one to avoid duplicated alocations
+    return (device_bank[tensor.data()] = iter_cpu->second);
+}
+
+bool Bank::decide_for_dev(const ov::Tensor &t, const std::string &) {
+    // Called in context of allocation on the device.
+    // Decide if this particular tensor should map to device or not.
+
+    // Let the limit be hardcoded for now - to 1.7 GB
+    const std::size_t DEV_LIMIT = 1700 * 1024 * 1024;
+
+    NPUW_ASSERT(m_bytes.total_registered != 0);
+    const std::size_t tbytes = t.get_byte_size();
+
+    if (DEV_LIMIT >= m_bytes.total_registered) {
+        // All allocations fit the limit - so it is an easy check
+        return true;
+    }
+    if (m_bytes.pinned_dev == 0 && tbytes <= DEV_LIMIT) {
+        // First allocation - let it be
+        return true;
+    }
+
+    // Ratio is required to maintain the device tensor distribution across the network
+    const float target_ratio = static_cast<float>(DEV_LIMIT) / m_bytes.total_registered;
+    const float tobe_ratio = static_cast<float>(m_bytes.pinned_dev + tbytes) / m_bytes.pinned_total;
+
+    if (tobe_ratio <= target_ratio) {
+        // The resulting ratio is within treshold - let it be on device
+        return true;
+    } else {
+        // The resulting ratio is above threshold - keep it to CPU
+        return false;
+    }
 }
 
 std::shared_ptr<Bank> BankManager::getBank(const std::string& bank_name, const std::shared_ptr<const ov::ICore>& core) {
