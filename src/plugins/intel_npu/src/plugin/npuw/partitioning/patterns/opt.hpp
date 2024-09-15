1a97504ecf8a8aa7bfce34bd74601cc5e84a4e4b
Dmitry Matveev <dmitry.matveev@intel.com>
2024-09-15 21:09:32 +0000

    NPUW Regather: Asym MM via Bcast, slow

--- src/plugins/intel_npu/src/plugin/npuw/partitioning/patterns/opt.hpp
+++ src/plugins/intel_npu/src/plugin/npuw/partitioning/patterns/opt.hpp
@@ -36,6 +36,11 @@ struct Context {
     using Ref = std::reference_wrapper<Context>;
 };
 
+class DQMatMulCWu : public ov::pass::MatcherPass {
+public:
+    DQMatMulCWu();
+};
+
 class DQMatMulGQi : public ov::pass::MatcherPass {
 public:
     explicit DQMatMulGQi(Context::Ref ctx);
