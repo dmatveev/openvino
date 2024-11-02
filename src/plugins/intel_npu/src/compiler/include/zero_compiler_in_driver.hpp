94df27e40554af61115ccb32a0c87fb19cc4db90
Dmitry Matveev <dmitry.matveev@intel.com>
2024-11-02 22:05:11 +0000

    Revert "[]NPU] Update adapter and backend to use new level zero ext table (#27095)"
    This reverts commit 837128968bf5c423dd7bd5d92d9167bf833516fc.

--- src/plugins/intel_npu/src/compiler/include/zero_compiler_in_driver.hpp
+++ src/plugins/intel_npu/src/compiler/include/zero_compiler_in_driver.hpp
@@ -18,34 +18,37 @@ namespace driverCompilerAdapter {
 
 using SerializedIR = std::pair<size_t, std::shared_ptr<uint8_t>>;
 
-#define NotSupportQuery(T) (T == ZE_GRAPH_EXT_VERSION_1_2)
+#define NotSupportQuery(T) (std::is_same<T, ze_graph_dditable_ext_1_2_t>::value)
 
 // ext version == 1.3 && 1.4, support API (pfnQueryNetworkCreate, pfnQueryNetworkDestroy,
 // pfnQueryNetworkGetSupportedLayers)
-#define SupportAPIGraphQueryNetworkV1(T) (T == ZE_GRAPH_EXT_VERSION_1_3 || T == ZE_GRAPH_EXT_VERSION_1_4)
+#define SupportAPIGraphQueryNetworkV1(T) \
+    (std::is_same<T, ze_graph_dditable_ext_1_3_t>::value || std::is_same<T, ze_graph_dditable_ext_1_4_t>::value)
 
 // ext version >= 1.5, support API (pfnCreate2, pfnQueryNetworkCreate2, pfnQueryContextMemory)
 #define SupportAPIGraphQueryNetworkV2(T) ((!NotSupportQuery(T) && !SupportAPIGraphQueryNetworkV1(T)))
 
 // For ext version >= 1.5, pfnCreate2 api is avaible
-#define NotSupportGraph2(T) \
-    (T == ZE_GRAPH_EXT_VERSION_1_2 || T == ZE_GRAPH_EXT_VERSION_1_3 || T == ZE_GRAPH_EXT_VERSION_1_4)
+#define NotSupportGraph2(T)                                                                                        \
+    (std::is_same<T, ze_graph_dditable_ext_1_2_t>::value || std::is_same<T, ze_graph_dditable_ext_1_3_t>::value || \
+     std::is_same<T, ze_graph_dditable_ext_1_4_t>::value)
 
 // A bug inside the driver makes the "pfnGraphGetArgumentMetadata" call not safe for use prior to
 // "ze_graph_dditable_ext_1_6_t".
 // See: E#117498
-#define NotSupportArgumentMetadata(T)                                                                   \
-    (T == ZE_GRAPH_EXT_VERSION_1_2 || T == ZE_GRAPH_EXT_VERSION_1_3 || T == ZE_GRAPH_EXT_VERSION_1_4 || \
-     T == ZE_GRAPH_EXT_VERSION_1_5)
+#define NotSupportArgumentMetadata(T)                                                                              \
+    (std::is_same<T, ze_graph_dditable_ext_1_2_t>::value || std::is_same<T, ze_graph_dditable_ext_1_3_t>::value || \
+     std::is_same<T, ze_graph_dditable_ext_1_4_t>::value || std::is_same<T, ze_graph_dditable_ext_1_5_t>::value)
 
-#define UseCopyForNativeBinary(T)                                                                       \
-    (T == ZE_GRAPH_EXT_VERSION_1_2 || T == ZE_GRAPH_EXT_VERSION_1_3 || T == ZE_GRAPH_EXT_VERSION_1_4 || \
-     T == ZE_GRAPH_EXT_VERSION_1_5 || T == ZE_GRAPH_EXT_VERSION_1_6)
+#define UseCopyForNativeBinary(T)                                                                                  \
+    (std::is_same<T, ze_graph_dditable_ext_1_2_t>::value || std::is_same<T, ze_graph_dditable_ext_1_3_t>::value || \
+     std::is_same<T, ze_graph_dditable_ext_1_4_t>::value || std::is_same<T, ze_graph_dditable_ext_1_5_t>::value || \
+     std::is_same<T, ze_graph_dditable_ext_1_6_t>::value)
 
 /**
  * Adapter to use CiD through ZeroAPI
  */
-template <ze_graph_ext_version_t TableExtension>
+template <typename TableExtension>
 class LevelZeroCompilerInDriver final : public ICompiler {
 public:
     LevelZeroCompilerInDriver(ze_driver_handle_t driverHandle,
@@ -76,7 +79,7 @@ public:
         OPENVINO_THROW("Profiling post-processing is not implemented.");
     }
 
-    template <ze_graph_ext_version_t T = TableExtension, std::enable_if_t<!NotSupportQuery(T), bool> = true>
+    template <typename T = TableExtension, std::enable_if_t<!NotSupportQuery(T), bool> = true>
     std::unordered_set<std::string> getQueryResultFromSupportedLayers(
         ze_result_t result,
         ze_graph_query_network_handle_t& hGraphQueryNetwork) const;
@@ -108,40 +111,35 @@ private:
                              ze_graph_compiler_version_info_t compilerVersion) const;
     std::string serializeConfig(const Config& config, ze_graph_compiler_version_info_t& compilerVersion) const;
 
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<NotSupportArgumentMetadata(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<NotSupportArgumentMetadata(T), bool> = true>
     void getMetadata(ze_graph_dditable_ext_curr_t& graphDdiTableExt,
                      ze_graph_handle_t graphHandle,
                      uint32_t index,
                      std::vector<IODescriptor>& inputs,
                      std::vector<IODescriptor>& outputs) const;
 
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<!NotSupportArgumentMetadata(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<!NotSupportArgumentMetadata(T), bool> = true>
     void getMetadata(ze_graph_dditable_ext_curr_t& graphDdiTableExt,
                      ze_graph_handle_t graphHandle,
                      uint32_t index,
                      std::vector<IODescriptor>& inputs,
                      std::vector<IODescriptor>& outputs) const;
 
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<UseCopyForNativeBinary(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<UseCopyForNativeBinary(T), bool> = true>
     void getNativeBinary(ze_graph_dditable_ext_curr_t& graphDdiTableExt,
                          ze_graph_handle_t graphHandle,
                          std::vector<uint8_t>& blob,
                          const uint8_t*& blobPtr,
                          size_t& blobSize) const;
 
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<!UseCopyForNativeBinary(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<!UseCopyForNativeBinary(T), bool> = true>
     void getNativeBinary(ze_graph_dditable_ext_curr_t& graphDdiTableExt,
                          ze_graph_handle_t graphHandle,
                          std::vector<uint8_t>& /* unusedBlob */,
                          const uint8_t*& blobPtr,
                          size_t& blobSize) const;
 
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<SupportAPIGraphQueryNetworkV2(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<SupportAPIGraphQueryNetworkV2(T), bool> = true>
     ze_result_t seriazlideIRModelAndQueryNetworkCreateV2(const std::shared_ptr<const ov::Model>& model,
                                                          const Config& config,
                                                          ze_device_graph_properties_t deviceGraphProperties,
@@ -149,13 +147,11 @@ private:
                                                          ze_graph_query_network_handle_t& hGraphQueryNetwork) const;
 
     // ext version >= 1.5, support API (pfnCreate2, pfnQueryNetworkCreate2, pfnQueryContextMemory)
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<SupportAPIGraphQueryNetworkV2(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<SupportAPIGraphQueryNetworkV2(T), bool> = true>
     std::unordered_set<std::string> queryImpl(const std::shared_ptr<const ov::Model>& model,
                                               const Config& config) const;
 
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<SupportAPIGraphQueryNetworkV1(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<SupportAPIGraphQueryNetworkV1(T), bool> = true>
     ze_result_t seriazlideIRModelAndQueryNetworkCreateV1(const std::shared_ptr<const ov::Model>& model,
                                                          const Config& config,
                                                          ze_device_graph_properties_t deviceGraphProperties,
@@ -164,24 +160,23 @@ private:
 
     // ext version == 1.3 && 1.4, support API (pfnQueryNetworkCreate, pfnQueryNetworkDestroy,
     // pfnQueryNetworkGetSupportedLayers)
-    template <ze_graph_ext_version_t T = TableExtension,
-              typename std::enable_if_t<SupportAPIGraphQueryNetworkV1(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<SupportAPIGraphQueryNetworkV1(T), bool> = true>
     std::unordered_set<std::string> queryImpl(const std::shared_ptr<const ov::Model>& model,
                                               const Config& config) const;
 
     // For ext version < 1.3
-    template <ze_graph_ext_version_t T = TableExtension, typename std::enable_if_t<NotSupportQuery(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<NotSupportQuery(T), bool> = true>
     std::unordered_set<std::string> queryImpl(const std::shared_ptr<const ov::Model>& model,
                                               const Config& config) const;
 
-    template <ze_graph_ext_version_t T = TableExtension, typename std::enable_if_t<NotSupportGraph2(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<NotSupportGraph2(T), bool> = true>
     ze_result_t createGraph(const ze_graph_format_t& format,
                             const SerializedIR& serializedIR,
                             const std::string& buildFlags,
                             const uint32_t& flags,
                             ze_graph_handle_t* graph) const;
 
-    template <ze_graph_ext_version_t T = TableExtension, typename std::enable_if_t<!NotSupportGraph2(T), bool> = true>
+    template <typename T = TableExtension, typename std::enable_if_t<!NotSupportGraph2(T), bool> = true>
     ze_result_t createGraph(const ze_graph_format_t& format,
                             const SerializedIR& serializedIR,
                             const std::string& buildFlags,
